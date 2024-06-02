d95d106972d89213e45c915d56b1d7d351ef4de9
Kooreis <kooreis.official@gmail.com>
2024-06-02 02:42:18 -0500

    This part of the script creates a 2D character array to represent the crossword puzzle. It then prompts the user to enter the crossword puzzle line by line. It reads each line and stores the characters in the 2D array.

--- solution.java
+++ solution.java
@@ -1,8 +1,8 @@
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the size of the crossword puzzle:");
-        int size = scanner.nextInt();
-        scanner.nextLine();
+char[][] crossword = new char[size][size];
+        System.out.println("Enter the crossword puzzle, line by line:");
+        for (int i = 0; i < size; i++) {
+            String line = scanner.nextLine();
+            for (int j = 0; j < size; j++) {
+                crossword[i][j] = line.charAt(j);
+            }
+        }
